f6a618fb0b2006f7ca312bcc2aaa52a0ef5d2502
Huf <bjhufstetler@gmail.com>
2022-05-27 15:57:07 -0700

    fix: updated knexfile.js

--- api/knexfile.js
+++ api/knexfile.js
@@ -3,49 +3,43 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+const HOST = process.env.DATABASE_HOST || '127.0.0.1';
+const USER = process.env.POSTGRES_USER || 'postgres';
+const PASSWORD = process.env.POSTGRES_PASSWORD || 'docker';
+const DATABASE = process.env.POSTGRES_DB || 'db';
+const PORT = process.env.PORT || 5432;
+
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
-      host: 'db',
-      password: 'docker',
-      user: 'postgres',
-      port: 5432,
-      database: 'db'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: HOST,
+      user: USER,
+      password: PASSWORD,
+      port: PORT,
+      database: DATABASE
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'db',
-      user:     'postgres',
-      password: 'docker',
-      port: 5432
-    },
+    connection: process.env.DATABASE_URL+'?ssl=no-verify',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './migrations'
+    },
+    seeds: {
+      directory: '.seeds'
     }
   }
 
