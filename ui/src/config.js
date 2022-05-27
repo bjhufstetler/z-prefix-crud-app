45b6dfdbfd781c17f18c2a4d09bdaec2d707367d
Huf <bjhufstetler@gmail.com>
2022-05-27 13:11:16 -0700

    fix: updates path variables for heroku deployment

---
+++ ui/src/config.js
@@ -0,0 +1,14 @@
+const config = {
+    development: {
+      apiUrl: 'http://localhost:8080',
+    },
+  
+    production: {
+      apiUrl: 'https://z-blog-api.herokuapp.com',
+    },
+    test: {
+      apiUrl: '',
+    },
+  };
+  
+  export default config;
